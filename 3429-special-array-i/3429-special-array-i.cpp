85abef42dc64463feb46fa131ca23d5b24734f53
Anshita Siroya <22bma007@nith.ac.in>
2025-02-02 00:16:28 +0530

    Time: 0 ms (100%), Space: 27.8 MB (2.79%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-        int sum  = 0;
         for(int j = 0; j<nums.size()-1; j++){
-            sum = nums[j]+nums[j+1];
+            int sum = nums[j]+nums[j+1];
             if(sum % 2 == 0) return false;
         }
         return true;
