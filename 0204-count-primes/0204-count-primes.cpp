fe9f99e2a740f7a4aa99aaa5c1aeaad5b868c46e
Anshita Siroya <22bma007@nith.ac.in>
2025-02-03 15:57:20 +0530

    Time: 246 ms (23.49%), Space: 12.7 MB (39.97%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<int> prime(n, 1);
+        vector<bool> prime(n, 1);
         for (int i = 2; i < n; i++) prime[i] = 1;
         for(int i = 2; i*i<n; i++){
             if(prime[i] == 1){
