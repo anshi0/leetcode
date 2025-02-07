da2c56597ea48c6b2d87fef192e1a1abd49b4a0d
Anshita Siroya <22bma007@nith.ac.in>
2025-02-07 16:31:23 +0530

    Time: 3 ms (76.24%), Space: 28.4 MB (62.2%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        
-        stack<int>st;
+
+
+/// this has a time complexity of o(N^2) and SC of O(N)
+        //   vector<int> nge(nums.size(), -1);
+        //   for (int i = 0; i < nums.size(); i++) {
+        //     for (int j = i+1; j < i+nums.size(); j++) {
+        //         int index = j % nums.size();
+        //         if (nums[index] > nums[i]) {
+        //             nge[i] = nums[index];
+        //             break;
+        //         }
+        //     }
+        //   }
+        //   return nge;
+
+
+
         int n = nums.size();
-        vector<int> nge(n,-1);
-        for(int i = 2*n-1; i>=0; i--){
-            while(!st.empty() && st.top()<=nums[i%n]) st.pop();
-            if(i<n){
-                nge[i] = st.empty()?-1:st.top();
-            }
-            st.push(nums[i%n]);
+        vector<int> res(n, -1);
+        stack<int> st;
+        for(int i = n * 2 - 1; i >=0 ; i--) {
+            while (!st.empty() && st.top() <= nums[i % n]) {
+            st.pop();
         }
-        //reverse(nge.begin(),nge.end());
-        return nge;
+        
+        // Only store results for the first pass (i < n)
+        if (i < n) {
+            if (!st.empty()) {
+                res[i] = st.top();
+            }
+        }
+        st.push(nums[i % n]);           
+        }
+        return res;
     }
 };
