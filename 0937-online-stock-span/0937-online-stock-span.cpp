f3b7d5a7ef1017eef2e40cffc599e7147ad1863c
Anshita Siroya <22bma007@nith.ac.in>
2025-02-11 15:39:27 +0530

    Time: 32 ms (55.81%), Space: 91.9 MB (88.13%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.cpp
+++ 0937-online-stock-span/0937-online-stock-span.cpp
@@ -1,18 +1,18 @@
 class StockSpanner {
 public:
-vector<int> arr;
+    stack<pair<int, int>> st;
+    int ind = -1;
     StockSpanner() {
-        
+        ind = -1;
+        //st.clear();
     }
     
     int next(int price) {
-        arr.push_back(price);
-        int cnt = 1;
-        for(int i = arr.size()-2; i>=0; i--){
-            if(arr[i] <= price) cnt++;
-            else break;
-        }
-        return cnt;
+        ind = ind+1;
+        while(!st.empty() && st.top().first<=price) st.pop();
+        int ans = ind - (st.empty()?-1:st.top().second);
+        st.push({price, ind});
+        return ans;
     }
 };
 
