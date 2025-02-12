8ebd218464094054faf050830c852b8948e2c297
Anshita Siroya <22bma007@nith.ac.in>
2025-02-12 06:05:12 +0530

    Time: 2 ms (40.84%), Space: 9.2 MB (90.63%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        int partLen = part.size();
-        string result;
-        for (char c : s) {
-            result.push_back(c);
-            if (result.size() >= partLen && result.substr(result.size() - partLen) == part) {
-                result.erase(result.size() - partLen, partLen);
-            } }
-        return result;
-    } };
+        while(s.length() !=0 && s.find(part)<s.length()){
+            s.erase(s.find(part), part.length());
+        }
+        return s;
+    }
+};
