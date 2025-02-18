cf82f7e0e6d1e8de380821f790890dc796f83d23
Anshita Siroya <22bma007@nith.ac.in>
2025-02-18 17:55:52 +0530

    Time: 0 ms (100%), Space: 19 MB (75.69%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -18,13 +18,13 @@ public:
         q.push(root);
         while(!q.empty()){
             int size = q.size();
-            vector<int>level;
+            //vector<int>level;
             for(int i =0; i<size; i++){
                 TreeNode* node = q.front();
                 q.pop();
                 if(node->left != NULL) q.push(node->left);
                 if(node->right != NULL) q.push(node->right);
-                level.push_back(node->val);
+                //level.push_back(node->val);
             }
             cnt++;
         }
