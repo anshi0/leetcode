dbe4f0b2c22a0ddfed698e0627ceb28c9d5fccd3
Anshita Siroya <22bma007@nith.ac.in>
2025-02-20 23:39:38 +0530

    Time: 3 ms (26%), Space: 12.8 MB (60.71%) - LeetHub

---
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool permutation(map<string, int> &mpp, string &s, int n){
+        if(s.size() == n){
+            if(mpp.find(s) == mpp.end()) return true;
+            return false;
+        }
+        s.push_back('0');
+        if(permutation(mpp, s, n)) return true;
+        s.pop_back();
+
+        s.push_back('1');
+        if(permutation(mpp, s, n)) return true;
+        s.pop_back();
+        return false;
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        map<string, int> mpp;
+        int size = nums.size();
+        for(int i =0; i<nums.size(); i++){
+            mpp[nums[i]]++;
+        }
+        string s = "";
+        permutation(mpp, s, size);
+        return s;
+    }
+};
