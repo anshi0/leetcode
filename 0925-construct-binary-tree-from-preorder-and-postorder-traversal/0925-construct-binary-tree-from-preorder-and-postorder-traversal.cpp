127010980fdc0c75a5e62a581a53271c0635502b
Anshita Siroya <22bma007@nith.ac.in>
2025-02-24 00:43:10 +0530

    Time: 11 ms (5.29%), Space: 28.6 MB (21.54%) - LeetHub

--- 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,18 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int index = 0;
-
     TreeNode* build(vector<int>& pre, vector<int>& post, int s, int e, unordered_map<int, int>& postMap) {
         if (s > e || index >= pre.size()) return NULL;
-
         TreeNode* root = new TreeNode(pre[index++]);
         if (s == e) return root;
-
         int ni = postMap[pre[index]];  // O(1) lookup instead of O(N) search
-
         root->left = build(pre, post, s, ni, postMap);
         root->right = build(pre, post, ni + 1, e - 1, postMap);
-
         return root;
     }
 
