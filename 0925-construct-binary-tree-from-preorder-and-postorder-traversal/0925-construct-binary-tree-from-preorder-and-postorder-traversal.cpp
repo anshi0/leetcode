c059eb5e251cebadd7eab50c3ddf065db0ad1bdb
Anshita Siroya <22bma007@nith.ac.in>
2025-02-24 00:41:23 +0530

    Time: 0 ms (100%), Space: 28.6 MB (21.54%) - LeetHub

--- 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,38 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* construct(vector<int>& preorder, vector<int>& postorder, int& index, int l, int h, unordered_map<int, int>& postorderMap) {
-        if (index >= preorder.size() || l > h) return nullptr;
+    int index = 0;
 
-        TreeNode* root = new TreeNode(preorder[index++]);
-        if (l == h) return root;
+    TreeNode* build(vector<int>& pre, vector<int>& post, int s, int e, unordered_map<int, int>& postMap) {
+        if (s > e || index >= pre.size()) return NULL;
 
-        // Find left child index in postorder
-        int leftChildIdx = postorderMap[preorder[index]];
+        TreeNode* root = new TreeNode(pre[index++]);
+        if (s == e) return root;
 
-        // Recursively construct left and right subtrees
-        root->left = construct(preorder, postorder, index, l, leftChildIdx, postorderMap);
-        root->right = construct(preorder, postorder, index, leftChildIdx + 1, h - 1, postorderMap);
-        
+        int ni = postMap[pre[index]];  // O(1) lookup instead of O(N) search
+
+        root->left = build(pre, post, s, ni, postMap);
+        root->right = build(pre, post, ni + 1, e - 1, postMap);
+
         return root;
     }
 
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
-        unordered_map<int, int> postorderMap;
+        unordered_map<int, int> postMap;
         for (int i = 0; i < postorder.size(); i++) {
-            postorderMap[postorder[i]] = i;
+            postMap[postorder[i]] = i;
         }
-        int index = 0;
-        return construct(preorder, postorder, index, 0, preorder.size() - 1, postorderMap);
+        return build(preorder, postorder, 0, preorder.size() - 1, postMap);
     }
 };
